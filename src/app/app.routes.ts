d54d4e3c1fe6021d1f3703a126c0d4e4510267c3
zacharyb12 <boualemzachary89@yahoo.fr>
2026-04-10 16:31:13 +0200

    validation navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './signin-component/signin-component';
+import { LoginComponent } from './login/login-component/login-component';
+import { Homepage } from './homepage/homepage';
+import { Maliste } from './maliste/maliste';
+import { ProductDetail } from './product-detail/product-detail';
+import { ProfileComponent } from './profile/profile';
 
 export const routes: Routes = [
-    {path: "signin-component", component : SigninComponent},
+    {path : '' , component : Homepage},
+    {path:'login' , component : LoginComponent},
+    {path : 'profile' , component : ProfileComponent},
+    {path:'register' , component : SigninComponent},
+    {path:'list' , component : Maliste},
+    {path : 'details' , component: ProductDetail},
+
 ];
