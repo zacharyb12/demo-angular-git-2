bd94fe4d4455e8697c8b36c0d2ca7163ee96de7d
Messahzakari <zakari.messah@icloud.com>
2026-04-10 15:39:16 +0200

    feat: création de la navbar responsive avec Tailwind CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('demo-angular-git');
-}
+export class AppComponent {}
