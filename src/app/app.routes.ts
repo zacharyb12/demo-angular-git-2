41579a9f04ab81a36c968aed97c89b90992be81a
andriikucheriavyi <andrii.kucheriavyi1@gmail.com>
2026-04-10 15:59:40 +0200

    signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './signin-component/signin-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "signin-component", component : SigninComponent},
+];
